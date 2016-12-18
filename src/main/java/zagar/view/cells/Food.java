72eee429735cdb0e2fcfd5f31d18d8e468379a23
Samsonova Olga <fantomochka@gmail.com>
2016-12-19 01:12:20 +0300

    FOOD IS HEXAGON

--- src/main/java/zagar/view/cells/Food.java
+++ src/main/java/zagar/view/cells/Food.java
@@ -3,6 +3,7 @@ package main.java.zagar.view.cells;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 
 /**
@@ -11,5 +12,14 @@ import java.awt.geom.Point2D;
 public final class Food extends Cell {
     public Food(@NotNull Point2D coordinate, @NotNull Color color, float size) {
         super(coordinate, color, size);
+    }
+
+    @Override
+    protected void addShape(@NotNull Graphics2D g, @NotNull Point2D centerCoordinate) {
+        Polygon polygon = new Polygon();
+        for (int i = 0; i < 6; i++)
+            polygon.addPoint((int) (centerCoordinate.getX() + 10 * Math.cos(i * 2 * Math.PI / 6)),
+                    (int) (centerCoordinate.getY() + 10 * Math.sin(i * 2 * Math.PI / 6)));
+        g.fill(polygon);
     }
 }
