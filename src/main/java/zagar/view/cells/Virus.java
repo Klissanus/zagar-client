9af8104599fc89fce85a661abb6ff87ca6fc18fe
Samsonova Olga <fantomochka@gmail.com>
2016-12-19 11:51:05 +0300

    virus is beautiful

--- src/main/java/zagar/view/cells/Virus.java
+++ src/main/java/zagar/view/cells/Virus.java
@@ -1,5 +1,6 @@
 package main.java.zagar.view.cells;
 
+import main.java.zagar.Game;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
@@ -11,5 +12,23 @@ import java.awt.geom.Point2D;
 public final class Virus extends Cell {
     public Virus(@NotNull Point2D coordinate, float size) {
         super(coordinate, Color.GREEN, size);
+    }
+
+    @Override
+    protected void addShape(@NotNull Graphics2D g, @NotNull Point2D centerCoordinate) {
+        Polygon hexagon = new Polygon();
+        int size = 50;
+        int a = 2 * (100 / 8 + 10);
+        a = Math.min(a, 100);
+        for (int i = 0; i < a; i++) {
+            float pi = 3.14f;
+            int spike = 0;
+            if (i % 2 == 0) {
+                spike = (int) (20 * Math.min(Math.max(1, (100 / 80f)), 8) * Game.zoom);
+            }
+            hexagon.addPoint((int) (centerCoordinate.getX() + ((size + spike) / 2) * Math.cos(i * 2 * pi / a))
+                    + size / 2, (int) (centerCoordinate.getY() + ((size + spike) / 2) * Math.sin(i * 2 * pi / a)) + size / 2);
+        }
+        g.fillPolygon(hexagon);
     }
 }
